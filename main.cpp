09114ee7fb8fee064da4eac706a08f24fec560d6
DevashishJoshi <deva.neggi@gmail.com>
2017-08-14 10:30:36 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,7 @@
 #include<iostream>
 using namespace std;
 
-long long foo(long long x){
-	return 0.5*x*x + 1.5*x + 1;
-}
-
 int main(){
-	int T;
-	cin>>T;
-	long long ans[T];
-	for(int t=0;t<T;t++){
-		long long U,V;
-		cin>>U,V;
-		if(V>U)
-			ans[t]=foo(U+1)-1;
-		else ans[t]=foo(V);
-	}
-	for(int t=0;t<T;t++) cout<<ans[t]<<endl;
-
+	cout<<"Hello World"<<endl;
+	return 0;
 }
